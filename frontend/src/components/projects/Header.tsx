7ca77cbb6123d316fa232d651b3c1a8cf7e0619d
nekrosb <makarov.nikita.mn@gmail.com>
2026-05-08 13:15:29 +0200

    feat(frontend): user can delete and create projects on the page projects

--- frontend/src/components/projects/Header.tsx
+++ frontend/src/components/projects/Header.tsx
@@ -1,16 +1,22 @@
-import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "../Button";
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons'
+import { Button } from '../Button'
 
+type Props = {
+  onCreateProjectClick: () => void
+}
 
-
-export function Header() {
-
-    return (
-        <header className="header">
-            <div className="header__logo">⏱ Time Tracker</div>
-            <div className="header-actions">
-                <Button text="▶ create project" onClick={() => {}} classBtn="button--primary" icon={faRightToBracket} />
-            </div>
-        </header>
-    );
+export function Header({ onCreateProjectClick }: Props) {
+  return (
+    <header className="header">
+      <div className="header__logo">⏱ Time Tracker</div>
+      <div className="header-actions">
+        <Button
+          text="▶ create project"
+          onClick={onCreateProjectClick}
+          classBtn="button--primary"
+          icon={faRightToBracket}
+        />
+      </div>
+    </header>
+  )
 }
--- frontend/src/routes/projects.tsx
+++ frontend/src/routes/projects.tsx
@@ -1,52 +1,93 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useEffect, useState } from 'react'
-import { getProjects } from '../api/projectsApi'
+import { getProjects, deleteProject } from '../api/projectsApi'
 import type { Project } from '../types/projects'
 import { Header } from '../components/projects/Header'
 import { ProjectItem } from '../components/projects/ProjectItem.tsx'
-
+import { CreateProjectForm } from '../components/projects/formCreateProject'
 
 export const Route = createFileRoute('/projects')({
   component: Projects,
 })
 
 function Projects() {
-  const [error, setError] = useState<string | null>(null);
-  const [projects, setProjects] = useState<Project[]>([]);
-  
+  const [error, setError] = useState<string | null>(null)
+  const [projects, setProjects] = useState<Project[]>([])
+  const [screen, setScreen] = useState<'projects' | 'createProject'>('projects')
+
   async function fetchProjects() {
     try {
-      const cache = await getProjects();
-      setProjects(cache);
-      setError(null);
+      const cache = await getProjects()
+      setProjects(cache)
+      setError(null)
     } catch (error) {
-      console.error("Failed to fetch projects:", error);
-      setError("Failed to load projects. Please try again later.");
+      console.error('Failed to fetch projects:', error)
+      setError(`Failed to fetch projects. Please try again later. ${error}`)
     }
+  }
+
+  useEffect(() => {
+    void fetchProjects()
+  }, [])
+
+  async function handleDeleteProject(projectId: number) {
+    try {
+      await deleteProject(projectId)
+      await fetchProjects()
+    } catch (error) {
+      console.error('Failed to delete project:', error)
+      setError('Failed to delete project. Please try again later.')
     }
-    
-    useEffect(() => {
-      void fetchProjects();
-    }, []);
+  }
 
+  function openCreateProjectScreen() {
+    setScreen('createProject')
+  }
 
-  return (<>
-  <Header />
-  <div className="main-container">
-    <h1>Projects</h1>
-    <p>Welcome to the Projects page!</p>
-    {error && <div className="error">{error}</div>}
-    {projects.length === 0 && <p>No projects found.</p>}
-    {projects.length > 0 && !error && projects.map((project) => (
-      <ProjectItem
-        key={project.id}
-        description={project.description}
-        title={project.title}
-        onDelete={() => {}}
-        onEdit={() => {}}
-      />
-    ))}
+  function closeCreateProjectScreen() {
+    setScreen('projects')
+  }
 
-  </div>
-  </>)
+  async function handleProjectCreated() {
+    await fetchProjects()
+    setScreen('projects')
+  }
+
+  return (
+    <>
+      <Header onCreateProjectClick={openCreateProjectScreen} />
+      <div className="main-container">
+        {screen === 'projects' && (
+          <>
+            <h1>Projects</h1>
+            <p>Welcome to the Projects page!</p>
+            {error && <div className="error">{error}</div>}
+            {projects.length === 0 && <p>No projects found.</p>}
+            {projects.length > 0 &&
+              !error &&
+              projects.map((project) => (
+                <ProjectItem
+                  key={project.id}
+                  description={project.description}
+                  title={project.title}
+                  onDelete={() => {
+                    void handleDeleteProject(project.id)
+                  }}
+                  onEdit={() => {}}
+                />
+              ))}
+          </>
+        )}
+
+        {screen === 'createProject' && (
+          <CreateProjectForm
+            onClose={closeCreateProjectScreen}
+            onCreated={() => {
+              void handleProjectCreated()
+            }}
+          />
+        )}
+      </div>
+    </>
+  )
 }
